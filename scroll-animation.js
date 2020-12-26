d8c435bac724ff8b15709bdd5a1a67846719af7f
ownnnwo <ownnnwo@gmail.com>
2020-12-26 19:39:03 +0100

    initial commit

---
+++ scroll-animation.js
@@ -0,0 +1,23 @@
+const photoGrid = document.querySelector(".photo-grid");
+
+const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+  if (section.className !== "photo-section")
+    gsap.fromTo(
+      section.children,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        stagger: 0.2,
+        duration: 2,
+        scrollTrigger: { trigger: section, start: "top 60%" },
+      }
+    );
+});
+
+gsap.fromTo(
+  photoGrid.children,
+  { opacity: 0 },
+  { opacity: 1, stagger: 0.2, duration: 3 }
+);
---
+++ smooth-navigation.js
@@ -0,0 +1,42 @@
+function smoothScroll(target, duration) {
+  let toMove = document.querySelector(target);
+  let toMovePosition = toMove.getBoundingClientRect().top;
+  let startPosition = window.pageYOffset;
+  let distance = toMovePosition - startPosition;
+  let startTime = null;
+
+  function animationScroll(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    let timeElapsed = currentTime - startTime;
+    let run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animationScroll);
+  }
+
+  //Function for easing
+  function ease(t, b, c, d) {
+    t /= d;
+    return -c * t * (t - 2) + b;
+  }
+
+  requestAnimationFrame(animationScroll);
+}
+
+let about = document.querySelector(".about");
+
+about.addEventListener("click", () => {
+  smoothScroll(".about-section", 700);
+});
+
+
+let work = document.querySelector(".work");
+
+work.addEventListener("click", () => {
+  smoothScroll(".work-section", 1400);
+});
+
+let contact = document.querySelector(".contact");
+
+contact.addEventListener("click", () => {
+  smoothScroll(".contact-section", 2100);
+});
