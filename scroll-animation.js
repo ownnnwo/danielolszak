3eab689170a5cbf6b6f8df0279299d91bd705b8f
ownnnwo <ownnnwo@gmail.com>
2021-01-03 20:20:04 +0100

    small changes

--- scroll-animation.js
+++ scroll-animation.js
@@ -1,39 +1,35 @@
+
+
 const photoGrid = document.querySelector(".photo-grid");
 const sections = document.querySelectorAll("section");
 
 sections.forEach((section) => {
-  
-if(section.className !== "photo-section"){
-  console.log(section.className);
+  if (section.className !== "photo-section") {
+    console.log(section.className);
 
     const animation = gsap.fromTo(
       section.children,
-      { opacity: 0 
-      },
+      { opacity: 0 },
       {
         opacity: 1,
-        stagger: 0.2,
-        duration: 0.5,
+        duration: 1.5,
         paused: true,
-        
+
         //scrollTrigger: { trigger: section, start: "top 60%"},
       }
     );
 
-  ScrollTrigger.create({
-    trigger: section,
-    start: "top 50%",
-    end: "30%",
+    ScrollTrigger.create({
+      trigger: section,
+      start: "top 50%",
+      end: "30%",
 
-    onToggle: () => animation.play(),
-    onLeave: () => animation.reverse(),
-    onLeaveBack: () => animation.reverse(),
-
-
-  });
-}
-
+      onToggle: () => animation.play(),
+      onLeave: () => animation.reverse(),
+      onLeaveBack: () => animation.reverse(),
+    });
+  }
 });
 
-
+  gsap.fromTo(photoGrid.children, { opacity: 0, }, { opacity: 1, stagger:0.2, duration:2 });
 
